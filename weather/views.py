f3a934ecaff5217ecf98a422c75d178c7f13145a
omid-mousavi <omidmousavi266@gmail.com>
2023-08-03 18:35:11 +0330

    completing

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+import urllib.request
+import json
 # Create your views here.
 def index(request):
-  return render(request, 'index.html')
+  json_dic = {}
+  if request.method == 'POST':
+    city = request.POST['city']
+    my_url = urllib.request.urlopen('http://api.openweathermap.org/geo/1.0/direct?q='+ city + '&appid=9cda8a49405d88e9e5794a5ce30769ff').read()
+    json_data = json.loads(my_url)
+    json_dic = {
+      'country': str(json_data[0]['state'])
+    }
+  return render(request, 'index.html', json_dic)
