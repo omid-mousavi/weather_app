5dd0730adf338747d180ce5b21937982e1fc44f0
omid-mousavi <omidmousavi266@gmail.com>
2023-08-04 19:22:12 +0330

    Completed

--- weather/views.py
+++ weather/views.py
@@ -6,9 +6,11 @@ def index(request):
   json_dic = {}
   if request.method == 'POST':
     city = request.POST['city']
-    my_url = urllib.request.urlopen('http://api.openweathermap.org/geo/1.0/direct?q='+ city + '&appid=9cda8a49405d88e9e5794a5ce30769ff').read()
+    my_url = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid=9cda8a49405d88e9e5794a5ce30769ff').read()
     json_data = json.loads(my_url)
     json_dic = {
-      'country': str(json_data[0]['state'])
+      'tempereture': str(json_data['main']['temp']),
+      'humidity': str(json_data['main']['humidity']),
+      'weather': str(json_data['weather'][0]['description'])
     }
   return render(request, 'index.html', json_dic)
